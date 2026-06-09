d60908a09e820b8ad56b283e18492d793e9f85e5
Mohamed Raffik <raffikmohamed7@gmail.com>
2026-06-09 02:12:46 -0400

    test(test_user): fix test cookies and webdav update tests to handle encrypted values

--- tests/routes/cookies/test_update_cookies.py
+++ tests/routes/cookies/test_update_cookies.py
@@ -62,4 +62,4 @@ async def test_update_cookies_with_existing(
     assert response.json() == updated_data
 
     await db_session.refresh(stored_cookies)
-    assert stored_cookies.cookies == updated_data["cookies"]
+    assert Cookies.decrypt_value(stored_cookies.cookies) == updated_data["cookies"]
